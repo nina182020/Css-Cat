8dd113830a01f8a64e579a023858f3808ca5998d
Nina <ninamaxcy@gmail.com>
2021-02-08 16:38:38 -0600

    working leg movement

--- Cat.js
+++ Cat.js
@@ -1,6 +1,2 @@
 const piece = document.querySelector('.piece');
-function solidPiece() {
-    const solidPiece = document.createElement('div');
-    solidPiece.classList.add('solidPiece');
-    piece.appendChild(solidPiece);
-};
+
